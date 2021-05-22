38a67387622512bba4724d3cd67f3f1ff4b882c4
Keith Patarroyo <keithypts@gmail.com>
2021-05-22 14:26:44 -0400

    test

--- content/database/block/zoom.js
+++ content/database/block/zoom.js
@@ -1,20 +1,20 @@
-var w = $(window).width();
-var h = $(window).height();
+var w = $("#myiframe").width();
+var h = $("#myiframe").height();
 var scale = 1;
 
 function zoom(x) {
   if (x === -1) {
     scale = scale * 1.1;
-    w = w * 0.9;
-    h = h * 0.9;
-    $("#myiframe").width(w + "px");
-    $("#myiframe").height(h + "px")
-  } else {
-    scale = scale * 0.9;
     w = w * 1.1;
     h = h * 1.1;
     $("#myiframe").width(w + "px");
-    $("#myiframe").height(h + "px")
+    $("#myiframe").height(h + "px");
+  } else {
+    scale = scale * 0.9;
+    w = w * 0.9;
+    h = h * 0.9;
+    $("#myiframe").width(w + "px");
+    $("#myiframe").height(h + "px");
   }
 
   $('#myiframe').css('transform', `scale(${scale})`);
