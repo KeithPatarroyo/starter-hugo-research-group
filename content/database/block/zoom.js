f34a59c6517daf7a5c389d4b303666eca599af70
Keith Patarroyo <keithypts@gmail.com>
2021-05-22 14:10:58 -0400

    Update zoom.js

--- content/database/block/zoom.js
+++ content/database/block/zoom.js
@@ -5,16 +5,8 @@ var scale = 1;
 function zoom(x) {
   if (x === -1) {
     scale = scale * 1.1;
-    w = w * 0.9;
-    h = h * 0.9;
-    $("#myiframe").width(w + "px");
-    $("#myiframe").height(h + "px")
   } else {
     scale = scale * 0.9;
-    w = w * 1.1;
-    h = h * 1.1;
-    $("#myiframe").width(w + "px");
-    $("#myiframe").height(h + "px")
   }
 
   $('#myiframe').css('transform', `scale(${scale})`);
