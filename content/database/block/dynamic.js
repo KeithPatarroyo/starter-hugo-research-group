3740f2d3b199657c59533528f867902a9b944860
Keith Patarroyo <keithypts@gmail.com>
2021-05-22 00:52:00 -0400

    test

--- content/database/block/dynamic.js
+++ content/database/block/dynamic.js
@@ -1,16 +1,3 @@
-//get the elements
-const canvas = document.getElementById("canvas");
-const myDiv = document.getElementById("my-div");
-var ctx =canvas.getContext("2d")
-
-//set the width and height attributes to the div width and height
-function resize(){
-  ctx.scale( myDiv.clientWidth/240,myDiv.clientHeight/460);
-}
-//on page resize, call resize()
-window.addEventListener("resize", resize, false);
-
-//call resize() initially to set the canvas size correctly
-resize();
-
-//you can call resize() when your div changes size, dynamically resizing the canvas to the div
+var canvas = document.getElementById('responsive-canvas');
+var heightRatio = 1.5;
+canvas.height = canvas.width * heightRatio;
