0f9b9296091766972899af7579c46c4cdb09b093
Keith Patarroyo <keithypts@gmail.com>
2021-05-21 20:05:26 -0400

    script

--- content/database/block/dynamic.js
+++ content/database/block/dynamic.js
@@ -1,6 +1,6 @@
-function init() {
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext('2d');
-  ctx.canvas.width  = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
-}
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
