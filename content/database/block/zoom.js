e4dd4fccc84b801c1aeb9631087a7f153ff157f5
Keith Patarroyo <keithypts@gmail.com>
2021-05-22 15:32:19 -0400

    Update zoom.js

--- content/database/block/zoom.js
+++ content/database/block/zoom.js
@@ -1,5 +1,5 @@
 var w = document.getElementById("myiframe").width
-//var h = document.getElementById("myiframe").height
+var h = document.getElementById("myiframe").height
 var scale = 1;
 
 function zoom(x) {
