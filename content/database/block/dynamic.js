900c3835e7a8598e196961f8f92a736db084218f
Keith Patarroyo <keithypts@gmail.com>
2021-05-21 21:09:45 -0400

    Update dynamic.js

--- content/database/block/dynamic.js
+++ content/database/block/dynamic.js
@@ -12,25 +12,21 @@ document.getElementsByTagName('head').item(0).appendChild(script);
 /* Include Many js files */
 include('https://galapagos.netlify.app/database/block/lv-plugin-1.js');
 
-(function() {
-    var canvas = document.getElementById('canvas'),
-            context = canvas.getContext('2d');
+var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d');
 
-    // resize the canvas to fill browser window dynamically
-    window.addEventListener('resize', resizeCanvas, false);
+// resize the canvas to fill browser window dynamically
+window.addEventListener('resize', resizeCanvas, false);
 
-    function resizeCanvas() {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
+function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
-            /**
-             * Your drawings need to be inside this function otherwise they will be reset when 
-             * you resize the browser window and the canvas goes will be cleared.
-             */
-            
-            draw();
-    }
-    resizeCanvas();
+        /**
+         * Your drawings need to be inside this function otherwise they will be reset when 
+         * you resize the browser window and the canvas goes will be cleared.
+         */
 
-
-})();
+        draw();
+}
+resizeCanvas();
