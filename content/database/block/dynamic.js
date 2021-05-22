2d45af8568931a6f2953185e299c7b8f4a5ab46d
Keith Patarroyo <keithypts@gmail.com>
2021-05-21 20:16:48 -0400

    Update dynamic.js

--- content/database/block/dynamic.js
+++ content/database/block/dynamic.js
@@ -1,6 +1,23 @@
+(function() {
+    var canvas = document.getElementById('canvas'),
+            context = canvas.getContext('2d');
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext('2d');
-ctx.canvas.width  = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
 
+    function resizeCanvas() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            /**
+             * Your drawings need to be inside this function otherwise they will be reset when 
+             * you resize the browser window and the canvas goes will be cleared.
+             */
+            drawStuff(); 
+    }
+    resizeCanvas();
+
+    function drawStuff() {
+            // do your drawing stuff here
+    }
+})();
