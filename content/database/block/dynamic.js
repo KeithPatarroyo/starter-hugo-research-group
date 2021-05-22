e1b423dcbf1ab70897eb1249202b93e92443bfbb
Keith Patarroyo <keithypts@gmail.com>
2021-05-21 20:50:16 -0400

    Update dynamic.js

--- content/database/block/dynamic.js
+++ content/database/block/dynamic.js
@@ -13,8 +13,8 @@
              * Your drawings need to be inside this function otherwise they will be reset when 
              * you resize the browser window and the canvas goes will be cleared.
              */
-            var sql = require("./lv-plugin-1");
-            sql.Ga();
+            import { draw } from 'lv-plugin-1.js';
+            draw();
     }
     resizeCanvas();
 
