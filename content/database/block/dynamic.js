83f47853c7d07a9b36350a9c8cfab57bbe6a22bc
Keith Patarroyo <keithypts@gmail.com>
2021-05-21 20:01:57 -0400

    test

--- content/database/block/dynamic.js
+++ content/database/block/dynamic.js
@@ -1,18 +1,6 @@
 function init() {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext('2d');
-  var wrapper = canvas.parentNode;
-  canvas.setAttribute("width", window.getComputedStyle(wrapper).width);
-  canvas.setAttribute("height", window.getComputedStyle(wrapper).height);
-
-  var max = 5;
-  var rectWidth = 300;
-  var rectHeight = 300;
-  canvas.setAttribute("width", (max) * rectWidth + "px");
-  canvas.setAttribute("height", (max) * rectHeight + "px");
-
-  for (i = 0; i < max; i++) {
-    ctx.fillRect(i * rectWidth, i * rectHeight, rectWidth, rectHeight);
-  }
-  ctx.fill();
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
 }
